09309339b85698b76243b457eb557dad19ccee2b
Łukasz Nosal <lukasz94nosal@gmail.com>
2019-01-17 15:16:09 +0100

    logging

--- django/blogdemo/views.py
+++ django/blogdemo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 class IndexView(View):
     template_name = 'blogdemo/index.html'
 
--- django/promosite/settings.py
+++ django/promosite/settings.py
@@ -130,3 +130,19 @@ STATICFILES_DIRS = [
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        '': {  # 'catch all' loggers by referencing it with the empty string
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
